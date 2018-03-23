a44a33590db45a4e4580e6f6993bb8a0bec90783
alpha <827130441@qq.com>
2018-03-23 10:31:52 +0800

    对称树完结

--- test.js
+++ test.js
@@ -7,14 +7,14 @@
      this.left = this.right = null;
  }
 
- var tree = new TreeNode(5)
- tree.left = new TreeNode(4)
- tree.right = new TreeNode(1)
+ var tree = new TreeNode(1)
+ tree.left = new TreeNode(2)
+ tree.right = new TreeNode(2)
 //  tree.left.left = new TreeNode(3)
- tree.left.right = new TreeNode(1)
+ tree.left.right = new TreeNode(3)
 //  tree.right.left = new TreeNode(2)
- tree.right.right = new TreeNode(4)
- tree.left.right.left = new TreeNode(2)
- tree.right.right.left = new TreeNode(2)
+ tree.right.right = new TreeNode(3)
+//  tree.left.right.left = new TreeNode(2)
+//  tree.right.right.left = new TreeNode(2)
 
  console.log(isSymmetric(tree))
--- tree/mirrorTree.js
+++ tree/mirrorTree.js
@@ -1,48 +1,4 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {boolean}
- */
-/*var isSymmetric = function (root) {
-    if (!root) return true
-    var resultArr, i, resultArrLength, loopLength
-    var resolveToArr = function (node) {
-        var left = node.left,
-            right = node.right,
-            leftArr = rightArr = null,
-            result = []
-        if (!left && !right) return [node.val]
-        if (!!left) {
-            result = result.concat(resolveToArr(left))
-        } else {
-            result.push(left)
-        }
-        result.push(node.val)
-        if (!!right) {
-            result = result.concat(resolveToArr(right))
-        } else {
-            result.push(right)
-        }
-        return result
-    }
-    resultArr = resolveToArr(root)
-    console.log(resultArr)
-    resultArrLength = resultArr.length
-    if (resultArrLength % 2 !== 1) return false
-    for (i = 0, loopLength = (resultArrLength - 1) / 2; i < loopLength; ++i) {
-        if (resultArr[i] !== resultArr[resultArrLength - 1 - i]) return false
-    }
-    return true
-};*/
-
-
-/**
+/** 需要前序遍历
  * Definition for a binary tree node.
  * function TreeNode(val) {
  *     this.val = val;
@@ -59,33 +15,43 @@ var isSymmetric = function (root) {
     var leftArr = [],
         rightArr = [];
     var traverseLeft = function (node) {
+        // 前序遍历，先左后右
         var result = []
-        if (!node.left && !node.right) return [node.value]
+        if (!node.left && !node.right) return [node.val]
+        result.push(node.val)
         if (!!node.left) {
             result = result.concat(traverseLeft(node.left))
+        } else {
+            result.push(node.left)
         }
-        result.push(node.value)
         if (!!node.right) {
             result = result.concat(traverseLeft(node.right))
+        } else {
+            result.push(node.right)
         }
         return result
     }
     var traverseRight = function (node) {
+        // 前序遍历，先右后左
         var result = []
-        if (!node.left && !node.right) return [node.value]
+        if (!node.left && !node.right) return [node.val]
+        result.push(node.val)
         if (!!node.right) {
             result = result.concat(traverseRight(node.right))
+        } else {
+            result.push(node.right)
         }
-        result.push(node.value)
         if (!!node.left) {
             result = result.concat(traverseRight(node.left))
+        } else {
+            result.push(node.left)
         }
         return result
     }
-    if (!!root.left) leftArr.concat(traverseLeft(root.left))
-    if (!!root.right) rightArr.concat(traverseRight(root.right))
+    if (!!root.left) leftArr = leftArr.concat(traverseLeft(root.left))
+    if (!!root.right) rightArr = rightArr.concat(traverseRight(root.right))
     console.log(leftArr)
-    console.log(leftArr)
+    console.log(rightArr)
     if (leftArr.length !== rightArr.length) return false
     for (var i = 0, arrLength = leftArr.length; i < arrLength; ++i) {
         if (leftArr[i] !== rightArr[i]) return false
